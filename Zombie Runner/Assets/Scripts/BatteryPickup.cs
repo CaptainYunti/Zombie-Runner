ce5cadc750456f4cce3a13603025981ae978225b
Yunti <n.pawcio@gmail.com>
2022-10-19 03:43:18 +0200

    Light changes

---
+++ Zombie Runner/Assets/Scripts/BatteryPickup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryPickup : MonoBehaviour
+{
+
+    [SerializeField] float lightAngleToRestore = 80f;
+    [SerializeField] float lightIntensityToRestore = 1f;
+
+    GameObject player;
+    FlashLightSystem playerFlash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerFlash = player.GetComponentInChildren<FlashLightSystem>();
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerFlash.RestoreLightAngle(lightAngleToRestore);
+            playerFlash.RestoreLightIntensity(lightIntensityToRestore);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Zombie Runner/Assets/Scripts/FlashLightSystem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightSystem : MonoBehaviour
+{
+    [SerializeField] float lightDecay = .1f;
+    [SerializeField] float angleDecay = 1f;
+    [SerializeField] float minimumAngle = 40f;
+
+    Light myLight;
+
+    private void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+
+    private void Update()
+    {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+
+    public void RestoreLightAngle(float restoreAngle)
+    {
+        myLight.spotAngle = restoreAngle;
+    }
+
+    public void RestoreLightIntensity(float intensityAmount)
+    {
+        myLight.intensity += intensityAmount;
+    }
+
+
+    private void DecreaseLightIntensity()
+    {
+        if (lightDecay <= 0) return;
+
+        myLight.intensity -= lightDecay * Time.deltaTime;
+    }
+
+    private void DecreaseLightAngle()
+    {
+        if (angleDecay <= minimumAngle) return;
+
+        myLight.spotAngle -= angleDecay * Time.deltaTime;
+    }
+}
